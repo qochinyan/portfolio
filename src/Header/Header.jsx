dbc2ee45b4b55bffe6975111907e22dd27fac2bc
qochinyan <arenqochinyan5@gmail.com>
2022-12-05 23:42:13 +0400

    responsivity

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -1,13 +1,13 @@
 import "./Header.css";
 import { NavLink } from "react-router-dom";
-const Header = ({setDarkTheme,darkTheme}) => {
-    const handleCheck=(evt)=>{
-        if(evt.target.checked){
-            setDarkTheme(false)
-        }else{
-            setDarkTheme(true)
-        }
-    } 
+const Header = ({ setDarkTheme, darkTheme }) => {
+  const handleCheck = (evt) => {
+    if (evt.target.checked) {
+      setDarkTheme(false);
+    } else {
+      setDarkTheme(true);
+    }
+  };
   return (
     <div className={darkTheme ? "headerBox" : "headerBox light"}>
       <div className="innerHeader">
@@ -15,44 +15,52 @@ const Header = ({setDarkTheme,darkTheme}) => {
           <input onClick={handleCheck} type="checkbox" />
           <span class="slider round"></span>
         </label>
-        <ul>
-          <li>
-            <NavLink
-              to="/"
-              style={({ isActive }) => ({
-                borderBottom: isActive ? "1px solid white" : "none",
-                borderColor:darkTheme ? "white" : "black",
-                textDecoration: "none",
-                color: darkTheme ? "white" : "rgb(36,36,36)"
-              })}>
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/About"
-              style={({ isActive }) => ({
-                borderBottom: isActive ? "1px solid white" : "none",
-                textDecoration: "none",
-                borderColor:darkTheme ? "white" : "black",
-                color: darkTheme ? "white" : "rgb(36,36,36)"
-              })}>
-              About
-            </NavLink>
-          </li>
-          <li>
-            <NavLink
-              to="/Contact"
-              style={({ isActive }) => ({
-                borderBottom: isActive ? "1px solid white" : "none",
-                textDecoration: "none",
-                borderColor:darkTheme ? "white" : "black",
-                color: darkTheme ? "white" : "rgb(36,36,36)"
-              })}>
-              Contact
-            </NavLink>
-          </li>
-        </ul>
+        <div className={darkTheme ? "burger" : "light burger"}>
+        <input type="checkbox" />
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        <div className="list">
+          <ul>
+            <li>
+              <NavLink
+                to="/portfolio"
+                style={({ isActive }) => ({
+                  borderBottom: isActive ? "1px solid white" : "none",
+                  borderColor: darkTheme ? "white" : "black",
+                  textDecoration: "none",
+                  color: darkTheme ? "white" : "rgb(36,36,36)",
+                })}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/About"
+                style={({ isActive }) => ({
+                  borderBottom: isActive ? "1px solid white" : "none",
+                  textDecoration: "none",
+                  borderColor: darkTheme ? "white" : "black",
+                  color: darkTheme ? "white" : "rgb(36,36,36)",
+                })}>
+                About
+              </NavLink>
+            </li>
+            <li>
+              <NavLink
+                to="/Contact"
+                style={({ isActive }) => ({
+                  borderBottom: isActive ? "1px solid white" : "none",
+                  textDecoration: "none",
+                  borderColor: darkTheme ? "white" : "black",
+                  color: darkTheme ? "white" : "rgb(36,36,36)",
+                })}>
+                Contact
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
